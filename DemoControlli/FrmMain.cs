5f89318349cb95fa64ff83aca3477433181bbcdc
ric <rcrosato@gmail.com>
2020-12-04 13:20:35 +0100

    Aggiornamento del 04/12/2020

--- DemoControlli/FrmMain.cs
+++ DemoControlli/FrmMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace DemoControlli
@@ -81,17 +82,52 @@ namespace DemoControlli
 
             flpPulsanti.Controls.Clear();
 
+            // calcolo il numero di pulsanti per riga
+            int nBtn = Convert.ToInt32(Math.Ceiling(Math.Sqrt(n)));
+
+            // calcolo il lato del singolo pulsante
+            int wBtn = Convert.ToInt32(flpPulsanti.Width / nBtn);
+
+            Random g = new Random();
+
             for (int i = 0; i < n; i++)
             {
                 Button btn = new Button();
-                
+
                 // impostare btn in modo tale che sia quadrato, di colore casuale, con testo l'indice del ciclo +1; le dimenzioni del singolo pulsante devono essere tali che il numero totale di pulsanti riempia al massimo il flp
+
+                // imposto le dimensioni del pulsante (quadrato)
+                btn.Width = btn.Height = wBtn;
+
+                // imposto testo e dimensioni del font
+                btn.Text = (i+1).ToString();
+                btn.Font = new Font("Tahoma", wBtn / 4);
+                
+                // rendo piatto e senza bordi il pulsante
+                btn.Margin = new Padding(0);
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderSize = 0;
+
+                // impostiamo i colori del pulsante
+                btn.ForeColor = Color.White;
+                btn.BackColor = Color.FromArgb(g.Next(0, 256), g.Next(0, 256), g.Next(0, 256));
+
+
+                // aggiungiamo un gestore dell'evento Click ai pulsanti
+                btn.Click += Btn_Click;
 
                 // inserisce il pulsante nel FlowLayoutPanel
                 flpPulsanti.Controls.Add(btn);
 
                 pgbAvanzamento.PerformStep(); // avanza la progress bar di uno step
             }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button b;
+            b = (Button)sender;  // cast del sender (mittente dell'evento) a Button (perchè sappiamo con certezza che il sender sarà un Button)
+            MessageBox.Show("Hai premuto " + b.Text);            
         }
     }
 }
